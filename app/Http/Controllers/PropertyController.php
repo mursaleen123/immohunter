740d5740c1b9a23bca89b4f41d237507fec3134f
Mursaleen Umar <45422628+mursaleen123@users.noreply.github.com>
2024-02-29 18:52:47 +0500

    hi

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -80,9 +80,9 @@ class PropertyController extends Controller
         $user = Auth::user();
 
         if ($user->role === 'admin') {
-            $properties = Property::all();
+            $properties = Property::latest()->get();
         } elseif ($user->role === 'vendor') {
-            $properties = Property::where('user_id', $user->id)->get();
+            $properties = Property::where('user_id', $user->id)->latest()->get();
         }
 
         $vendors = DB::table('users')->where('role', '=', 'vendor')->get();
@@ -92,6 +92,7 @@ class PropertyController extends Controller
             'users' => $vendors,
         ]);
     }
+
     public function updateStatus(Request $request, $id)
     {
         $property = Property::where('id', $id)->update(['status' => $request->status]);
